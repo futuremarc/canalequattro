8637851a69bc674a440812f4c4d04fd750d0c4ef
futuremarc <mas1529@nyu.edu>
2017-05-08 22:39:08 -0400

    testing schedule

--- public/js/local/main.js
+++ public/js/local/main.js
@@ -28,7 +28,7 @@ var mic_sensitivity = 1.5,
     brightness = 0.
 
 var container
-var dates = ['8-5-2017 22:34:00 EDT', '7-5-2017 22:34:30 EDT', '7-5-2017 22:35:00 EDT', '9-5-2017 07:00:00 EDT', '9-5-2017 14:00:00 EDT', '9-5-2017 20:00:00 EDT', '9-5-2017 23:00:00 EDT'],
+var dates = ['8-5-2017 22:40:00 EDT', '7-5-2017 22:41:00 EDT', '7-5-2017 22:42:00 EDT', '9-5-2017 07:00:00 EDT', '9-5-2017 14:00:00 EDT', '9-5-2017 20:00:00 EDT', '9-5-2017 23:00:00 EDT'],
     interval = 1000,
     currentDuration,
     countdownInterval
