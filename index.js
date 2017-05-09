779a10a3b6158c0513e6bc2f2684ce7665c5ec1f
futuremarc <mas1529@nyu.edu>
2017-05-09 07:36:54 -0400

    sizing

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var path = require('path')
-var bodyParser = require('body-parser')
-var cookieParser = require('cookie-parser')
-var cron = require('node-cron')
-var io = require('socket.io')(5050, {
-  path: '/socket'
-})
-
 
 if (app.get('env') === 'development'){
   var config = require('./config/config-dev');
@@ -18,25 +11,11 @@ if (app.get('env') === 'development'){
   var config = require('./config/config');
 }
 
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-
-var crons = require('cronfile')()
-
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')))
 
-var mainRoutes = require('routes/main')();
-
-// app.use('/', mainRoutes);
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-
-var mainSockets = require('sockets')(io)
-
-var server = http.listen(8080, function() {
+var server = http.listen(3030, function() {
   console.log('listening on', this.address().port)
 })
